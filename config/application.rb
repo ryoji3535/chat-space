dd8d3649297a58b1018dfa65f4547bd7307509d8
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-03 00:34:42 +0900

    generater修正＆モデル作成

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,12 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    g.coffee    false
-    g.helper    false
-    g.scss      false
-    g.yml  false
+    config.generators do |g|
+     g.coffee    false
+     g.helper    false
+     g.scss      false
+     g.test_framework :rspec,fixture: false
+    end
   end
 end
+
