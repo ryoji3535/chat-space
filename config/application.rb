45ab579b110a9bcabe68d6e535e536a9242c31d0
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-02 09:06:32 +0900

    fixtures削除＆generater設定変更

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    g.coffee false
-    g.helper false
-    g.scss false
+    g.coffee    false
+    g.helper    false
+    g.scss      false
+    g.fixtures  false
   end
 end
