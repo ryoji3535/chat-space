76758d5fc5846810f1e49abd7313cd3e7914c2a4
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-07 22:37:09 +0900

    group作成機能追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,13 @@ class GroupsController < ApplicationController
   end
 
   def create
+    Group.create(group_params)
+    redirect_to  "/"
   end
 
+  private
+
+  def group_params
+  params.permit(:name)
+  end
 end
