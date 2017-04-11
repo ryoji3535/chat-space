9052cbf42cb1e4dd7e178caf4807910f9c8c4ad9
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-11 23:36:52 +0900

    index　グループ表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,16 @@ class MessagesController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @group = @user.groups
-    @ug = Group.find(@group[0][:id])
-    @ugo = @ug.users
-    @ugoo = []
-    num = 0
-    while num < @ugo.length
-      @ugoo << @ugo[num][:name]
-      num = num + 1
+    if @group.present?
+      @ug = Group.find(@group[0][:id])
+      @ugo = @ug.users
+      @ugoo = []
+      num = 0
+      while num < @ugo.length
+        @ugoo << @ugo[num][:name]
+        num = num + 1
+      end
     end
-    #@all_groups = Group.find(current_user.id).order("desc")
+      #@all_groups = Group.find(current_user.id).order("desc")
   end
 end
