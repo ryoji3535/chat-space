661734dd8fd8d122e00ff4b451c790062e122832
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-11 23:35:41 +0900

    group edit ルーティング整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "messages#index"
-  resources :groups, :except => [:index]
+  resources :groups, :except => [:index, :show, :update]
+  get '/groups/:id', to: 'groups#update'
   devise_for :users
 end
