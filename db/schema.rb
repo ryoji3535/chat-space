a92c1ba60c40cc06b74e59272f94117ea68696c9
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-11 23:35:18 +0900

    group name 一意制約

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20170408014012) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
