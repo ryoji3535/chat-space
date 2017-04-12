50c8d3dc7d1b1ecd173031344ad6a3cb67929193
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-13 03:16:24 +0900

    不要な記述削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,9 +8,11 @@ class GroupsController < ApplicationController
   end
 
   def update
+
     @group = Group.find(params[:id])
     @menber = @group.users
     @user = User.find_by(name: params[:user_name])
+
     if @group.name == params[:group_name]
       if @user.present?
         @user.user_groups.update(user_id: @user.id)
@@ -33,38 +35,15 @@ class GroupsController < ApplicationController
     @test_group = Group.find_by(name: gname)
     @user = User.find_by(name: uname)
 
-    if test_group.blank?
+    if @test_group.blank?
       @group = Group.create(name: gname)
       @group.user_groups.create(group_id: @group.id, user_id: current_user.id)
       if @user.present?
         @group.user_groups.create(group_id: @group.id, user_id: @user.id)
       end
     end
-    binding.pry
     redirect_to  "/"
   end
-#def create
-#    if Group.find_by(name: params[:group_name]).present?
-#    else
-#      @group = Group.create(group_params)
-#      @user = User.find_by(name: params[:user_name])
-#      if@user.present?
-#        user_ids = []
-#        user_ids << current_user.id
-#        user_ids << @user.id
-#          user_ids.each do |id|
-#            @group.user_groups.create(group_id: @group.id, user_id: id)
-#          end
-#       else
-#         @group.user_groups.create(group_id: @group.id, user_id: current_user.id)
-#      end
-#    end
-#    redirect_to  "/"
-#  end
 
-  private
-    def group_params
-      params.permit(:name)
-    end
 end
 
