1ecbb17ad9a9eebd67a5375fb7db92b46d54bdea
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-22 23:54:05 +0900

    インデックス　コントローラー修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,5 @@
 class MessagesController < ApplicationController
   def index
-    @group = current_user.groups
-    if @group.present?
-      @groups = Group.find(@group[0][:id])
-      @group_menber = @groups.users
-      @group_menber_names = []
-      num = 0
-      while num < @group_menber.length
-        @group_menber_names << @group_menber[num][:name]
-        num = num + 1
-      end
-    end
+    @groups = current_user.groups
   end
 end
