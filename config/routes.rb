3d8d4cff0bec3793ab8a626eebeb789ce00a463f
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-25 20:45:10 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "messages#index"
-  resources :groups, except: [:index, :show, :update]
-  get '/groups/:id', to: 'groups#update'
+  resources :groups, except: [:index, :show]
   devise_for :users
 end
