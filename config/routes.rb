71202f41762313fc601360e72f394013a1931b2a
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-28 07:58:53 +0900

    ルーティング整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "messages#index"
-  resources :groups, except: [:index, :show]
+  root "groups#index"
+  resources :groups, except: [:index, :show] do
+    resources :messages, only: [:index,:create]
+  end
   devise_for :users
 end
