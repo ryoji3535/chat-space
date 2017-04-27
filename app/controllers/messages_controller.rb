7cf8e4e11bba842fb0a8bbd05bedb9b582380da3
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-04-28 08:01:37 +0900

    messageコントローラー整理

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,17 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = @groups.first
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+    redirect_to "/groups/#{@group.id}/messages"
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:message)
   end
 end
