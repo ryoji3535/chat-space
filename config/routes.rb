9182a356337c3b38403b736684ae42362ea1751d
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-05-03 02:16:16 +0900

    migration　ファイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "groups#index"
   resources :groups, except: [:index, :show] do
     resources :messages, only: [:index,:create]
   end
-  devise_for :users
 end
--- db/migrate/20170427231633_add_details_to_messages.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToMessages < ActiveRecord::Migration[5.0]
-  def change
-    add_column :messages, :text, :text
-    add_column :messages, :image, :string
-    add_column :messages, :user_id, :integer
-    add_column :messages, :group_id, :integer
-  end
-end
