9c6bf2eb1c412e7312afb81ff426ec3c4e931780
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-05-11 13:41:32 +0900

    params からuser_id削除

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,61 +1,58 @@
 require 'rails_helper'
 
 describe MessagesController do
-    describe '有効なパラメータの場合' do
-      let(:user) { create(:user) }
+  describe '有効なパラメータの場合' do
+    let(:user) { create(:user) }
 
-      let(:params) do
+    let(:params) do
+      {
+        group_id: 2,
+        message: {
+        text: 'sample'
+        }
+      }
+    end
+    before do
+      login_user user
+    end
+
+    it 'データベースに新しいメッセージが登録されること' do
+      expect{
+        post :create, params: params, user_id: user[:id]
+      }.to change(Message, :count).by(1)
+    end
+
+    it 'indexにリダイレクトすること' do
+      post :create, params: params
+      expect(response).to redirect_to group_messages_path
+    end
+  end
+
+  describe '無効なパラメータの場合' do
+    let(:user) { create(:user) }
+
+    let(:params) do
         {
           group_id: 2,
-          user_id: 1,
           message: {
-          text: 'sample'
+          text: ''
           }
         }
-      end
-      before do
-        login_user user
-      end
-
-      it 'データベースに新しいユーザーが登録されること' do
-        expect{
-          post :create, params: params
-        }.to change(Message, :count).by(1)
-      end
-
-      it 'indexにリダイレクトすること' do
-        post :create, params: params
-        expect(response).to redirect_to group_messages_path
-      end
     end
 
-    describe '無効なパラメータの場合' do
-      let(:user) { create(:user) }
-
-      let(:params) do
-          {
-            group_id: 2,
-            user_id: 1,
-            message: {
-            text: ''
-            }
-          }
-      end
-
-      before do
-        login_user user
-      end
-
-      it 'データベースに新しいユーザーが登録されないこと' do
-        expect{
-          post :create, params: params
-        }.not_to change(Message, :count)
-      end
-
-      it 'indexにリダイレクトすること' do
-        post :create, params: params
-        expect(response).to redirect_to group_messages_path
-      end
-
+    before do
+      login_user user
     end
+
+    it 'データベースに新しいメッセージが登録されないこと' do
+      expect{
+        post :create, params: params, user_id: user[:id]
+      }.not_to change(Message, :count)
+    end
+
+    it 'indexにリダイレクトすること' do
+      post :create, params: params
+      expect(response).to redirect_to group_messages_path
+    end
+  end
 end
