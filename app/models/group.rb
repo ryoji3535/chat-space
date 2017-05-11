c3a8d19b459fd2c4f3a21e20ec345d784815d78d
sakoryoji\ryoji <ryoji.sako@todai-dream-net.com>
2017-05-12 02:07:30 +0900

    グループ作成　バリデーション追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
   has_many :user_groups
   has_many :messages
+  validates :name, presence: true, uniqueness: true
+  validates :user_groups, presence: true
 end
